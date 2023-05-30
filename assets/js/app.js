bdfc53b4259e6c26fc7b805f27b3c70206e59823
Gabriel Oliveira <gabriel.oliveira@newm.com.br>
2023-05-29 21:46:47 -0300

    Ultimas alterações

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,36 @@
-let nav = document.querySelector(".header__nav");
-let btn = document.querySelector(".header__elements__btn");
-btn.addEventListener("click", function () {
-  nav.classList.toggle("header__nav--closed");
-  btn.classList.toggle("header__elements__btn--closed");
-});
-//Slider usando slick
-$(document).ready(function () {
-  $('.slider').slick({
-    slidesToShow: 4,
-    arrows: false,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          dots: true,
-          slidesToShow: 3
+let texts = ['resultados.', 'inovação.', 'pessoas.', 'mobilidade.']; // textos pré-definidos
+let heading = document.querySelector(".title--animated"); // referencia do elemento
+//sortear um dos textos
+function sortNumber(texts) { // sorteia um numero qualquer
+    numberRand = Math.floor(Math.random() * texts.length);
+    return numberRand;
+}
+function typeWriter(element) { // referencia h1 do html5 
+    let textSort = ' ' + texts[sortNumber(texts)];
+    for (let index = 0; index < textSort.length; index++) {
+        setTimeout(() => {
+            element.innerHTML += textSort[index];
+        }, 200 * index);
+    }
+    retypeWriter(element)
+}
+//executo a função trocando o texto a cada 10s 
+setInterval(() => {
+    typeWriter(heading);
+}, 10000);
+//primeira execução
+typeWriter(heading);
+
+function retypeWriter(element) {
+    setTimeout(() => {
+        const words = element.innerHTML.split(' '); // separo o elemento e um array de palavras
+        const lastWord = words[2] + ' '; // coloco um espaço no final da ultima palavra
+        const arrayLastWord = lastWord.split(''); // transformo a palavra em um array de letras
+        for (let index = 0; index < arrayLastWord.length; index++) {
+            setTimeout(() => {
+                element.innerHTML = element.innerHTML.slice(0, -1);
+            }, 200 * index);
         }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: true,
-          dots: false,
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-});
+    }, 5000) //Após 3 segundos a função é executada.
+
+}
